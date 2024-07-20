b5ec799870448bf0fd5aa504d1c22371cd2102a6
daniel-hortencio <hortencio.daniel@gmail.com>
2024-07-20 13:17:25 -0300

    adjusts in multiple select

--- src/components/ui/custom/multiple-select.tsx
+++ src/components/ui/custom/multiple-select.tsx
@@ -31,12 +31,12 @@ export const MultipleSelect = ({ options = [], value, onChange }: Props) => {
         asChild
         className="border border-input transition-all bg-background hover:text-accent-foreground cursor-pointer"
       >
-        <div className="w-full h-fit min-h-9 p-1 flex flex-wrap gap-2 items-center">
+        <div className="w-full h-fit min-h-10 p-1 flex flex-wrap gap-2 items-center">
           {value.length > 0 ? (
             value.map((option) => (
               <span
                 key={option}
-                className="w-fit h-7 text-sm rounded-md pl-2 pr-0 flex items-center gap-1 bg-neutral-600 text-primary-foreground hover:bg-neutral-800"
+                className="w-fit h-7 text-sm rounded-full pl-2 pr-0 flex items-center gap-1 bg-neutral-200/80 text-neutral-700 font-medium hover:bg-neutral-300/80"
               >
                 {option}
                 <button
